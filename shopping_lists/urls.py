8b7ac7c68dc4bb5a497ba182de2719ad473d2243
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-03-30 22:42:59 +0300

    Feature, adds update shopping list name functionality - Handles editing of the shopping list via a modal form - Handles error handling for the action. [Delivers #155906721]

--- shopping_lists/urls.py
+++ shopping_lists/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DashBoardView
+from .views import DashBoardView, ShoppingListUpdate
 
 app_name = "shopping_lists"
 urlpatterns = [
-    path('', DashBoardView.as_view(), name='dashboard')
+    path('list_update/<pk>/', ShoppingListUpdate.as_view(), name='update'),
+    path('', DashBoardView.as_view(), name='dashboard'),
 ]
--- shopping_lists/views.py
+++ shopping_lists/views.py
@@ -1,9 +1,11 @@
 # from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect, render_to_response
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.views.generic.edit import FormMixin
 
 from shopping_lists.models import ShoppingList
 from .forms import ShoppingListForm
@@ -31,3 +33,21 @@ class DashBoardView(SuccessMessageMixin, CreateView):
             return super(DashBoardView, self).form_invalid(form)
         form.instance.owner = owner
         return super(DashBoardView, self).form_valid(form)
+
+
+class ShoppingListUpdate(SuccessMessageMixin, UpdateView):
+    model = ShoppingList
+    form_class = ShoppingListForm
+    template_name = "shopping_lists/edit_list_form.html"
+    success_url = reverse_lazy("shopping_lists:dashboard")
+    success_message = "Update was successful."
+
+    def form_valid(self, form):
+        list_name = form.cleaned_data['name']
+        owner = self.request.user
+        if ShoppingList.objects.filter(name=list_name, owner=owner).exists():
+            form._errors[' A Shopping List with that name already exists'] = ''
+            context = {"form":form}
+            context['shopping_lists'] = ShoppingList.objects.filter(owner=owner)
+            return render_to_response("shopping_lists/dashboard.html",context=context)
+        return super(ShoppingListUpdate, self).form_valid(form)
