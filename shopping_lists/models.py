e8168877a85b8686ce0b3b24e055884f3f28a2a1
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-03-30 16:52:50 +0300

    Featue Adds Form for adding a new shopping list: - Added a modelform for manipulating a shopping_list - added the form to the dashboard view and template [Delivers #155906705]

---
+++ shopping_lists/models.py
@@ -0,0 +1,18 @@
+from accounts.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+
+class ShoppingList(models.Model):
+    """This is the model for a single shopping list"""
+    name = models.CharField(_('list_name'), max_length=20, null=False)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='shopping_lists')
+    date_created = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
--- shopping_lists/views.py
+++ shopping_lists/views.py
@@ -1,11 +1,20 @@
 # from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+
+from .forms import ShoppingListForm
 
 # Create your views here.
 
 
 @method_decorator(login_required, name='dispatch')
-class DashBoardView(TemplateView):
+class DashBoardView(CreateView):
     template_name = 'shopping_lists/dashboard.html'
+    form_class = ShoppingListForm
+    success_url = reverse_lazy("shopping_lists:dashboard")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(DashBoardView, self).form_valid(form)
