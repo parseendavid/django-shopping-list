c35af6c0ed2251707032da807653c5f6dc120ed5
David Parseen Maitoyo <david.maitoyo@andela.com>
2018-04-01 18:55:30 +0300

    Feature Added functionality for deleting a shopping list. - Added a delete view for the user. - Added a modal to handle the delete confirmation.

--- shopping_lists/urls.py
+++ shopping_lists/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DashBoardView, ShoppingListUpdate
+from .views import DashBoardView, ShoppingListUpdate, ShoppingListDelete
 
 app_name = "shopping_lists"
 urlpatterns = [
     path('list_update/<pk>/', ShoppingListUpdate.as_view(), name='update'),
+    path('list_delete/<pk>/', ShoppingListDelete.as_view(), name='delete'),
     path('', DashBoardView.as_view(), name='dashboard'),
 ]
